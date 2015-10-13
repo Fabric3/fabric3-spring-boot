85fb9ee96811afed6036cac3d92158eef09f22c1
jmarino <jim.marino@gmail.com>
2015-10-14 01:17:28 +0200

    update to support reference injection

--- src/main/java/org/fabric3/spring/boot/Fabric3BeanPostProcessor.java
+++ src/main/java/org/fabric3/spring/boot/Fabric3BeanPostProcessor.java
@@ -1,12 +1,20 @@
 package org.fabric3.spring.boot;
 
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
 
 import org.fabric3.api.node.Domain;
 import org.fabric3.api.node.Fabric;
-import org.fabric3.api.node.ServiceIntrospector;
+import org.fabric3.api.node.service.InjectorFactory;
+import org.fabric3.api.node.service.ServiceIntrospector;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 /**
@@ -18,18 +26,38 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 public class Fabric3BeanPostProcessor implements BeanPostProcessor {
     private Domain domain;
     private ServiceIntrospector serviceIntrospector;
+    private InjectorFactory injectorFactory;
 
     private List<Runnable> deployments = new ArrayList<>();
 
     public Fabric3BeanPostProcessor(Fabric fabric) {
         this.domain = fabric.getDomain();
         this.serviceIntrospector = fabric.getSystemService(ServiceIntrospector.class);
+        this.injectorFactory = fabric.getSystemService(InjectorFactory.class);
     }
 
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         if (serviceIntrospector.exportsEndpoints(bean.getClass())) {
             // service is a remote endpoint; track it for deployment later when the Fabric3 remote transports are started
             deployments.add(() -> domain.deploy(beanName, bean));
+        }
+        Class<?> beanClass = bean.getClass();
+        Map<AccessibleObject, Supplier<Object>> injectors = injectorFactory.getInjectors(beanClass);
+        for (Map.Entry<AccessibleObject, Supplier<Object>> entry : injectors.entrySet()) {
+            try {
+                if (entry.getKey() instanceof Method) {
+                    Method method = (Method) entry.getKey();
+                    method.setAccessible(true);
+                    method.invoke(bean, entry.getValue().get());
+                } else if (entry.getKey() instanceof Field) {
+                    Field field = (Field) entry.getKey();
+                    field.setAccessible(true);
+                    field.set(bean, entry.getValue().get());
+
+                }
+            } catch (InvocationTargetException | IllegalAccessException e) {
+                throw new BeanInitializationException("Error injecting " + beanClass.getName(), e);
+            }
         }
         return bean;
     }
